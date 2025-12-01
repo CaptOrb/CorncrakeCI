d589336667d1513dbe172e0e5de538cbe6cf915f
CaptOrb <tehorb13@gmail.com>
2025-12-01 22:38:35 +0000

    fix comment

--- server/migrations/002_update_repo_tbl_webhooks.sql
+++ server/migrations/002_update_repo_tbl_webhooks.sql
@@ -1,5 +1,4 @@
 ALTER TABLE repositories
-    ADD COLUMN webhook_secret TEXT;
-    
 -- Secret for authenticating that a webhook request came from the forge.
 -- If NULL, means the webhook has not been set up yet.
+    ADD COLUMN webhook_secret TEXT;
