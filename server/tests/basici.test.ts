4b23d9f3d351adf4b0a183b614682225bf53a848
CaptOrb <tehorb13@gmail.com>
2025-12-04 22:48:59 +0000

    Adds supertest dependency

--- server/tests/basici.test.ts
+++ server/tests/basici.test.ts
@@ -1,6 +1,9 @@
 // just testing ts-jest
+//import request from "supertest";
+//import express from 'express';
+
 test("adds 1 + 2 to equal 3", (): void => {
-  const one: string = 1;
+  const one: number = 1;
   const two: number = 2;
   expect(one + two).toBe(3);
 });
