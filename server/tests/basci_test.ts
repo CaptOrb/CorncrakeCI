c4d0c62c42683365e756ebfa6ffc35e762e7e851
Olivier 'reivilibre <git.contact@librepush.net>
2025-12-04 16:43:59 +0000

    Fix test config and have separate tsconfig for testing
    Signed-off-by: Olivier 'reivilibre <git.contact@librepush.net>

--- server/tests/basci_test.ts
+++
@@ -1,4 +0,0 @@
-// just testing ts-jest
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
-});
---
+++ server/tests/basici.test.ts
@@ -0,0 +1,6 @@
+// just testing ts-jest
+test("adds 1 + 2 to equal 3", (): void => {
+  const one: string = 1;
+  const two: number = 2;
+  expect(one + two).toBe(3);
+});
