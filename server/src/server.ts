80344f93af1595ec89efeda9f8c62df8ade9b9b5
Olivier 'reivilibre <git.contact@librepush.net>
2025-12-09 11:02:55 +0000

    Don't have a separate migrate script
    Do migrations on server startup (and expose a migrate-only command)
    
    Signed-off-by: Olivier 'reivilibre <git.contact@librepush.net>

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,10 +8,13 @@ import { runJobs } from "./jobs/graphile-worker";
 import authRouter from "./routes/auth";
 import { seedForges } from "./util/seedforges";
 import { Pool } from "pg";
+import { migrate } from "postgres-migrations";
+import { resolve } from "path";
 
 async function startServer() {
 	const pool = new Pool({ connectionString: config.db.uri });
 	await connectDB(pool);
+	await runMigrations(pool);
 	await seedForges();
 
 	const app = express();
@@ -55,7 +58,42 @@ async function startServer() {
 	});
 }
 
-startServer().catch((err) => {
-	console.error("Failed to start server:", err);
+async function runMigrations(pool: Pool): Promise<void> {
+	const client = await pool.connect();
+	try {
+		await migrate({ client }, resolve(__dirname, "../migrations"), {
+			// Enable logging to see which migrations are being applied
+			logger: (msg) => console.log(`[Migration] ${msg}`),
+		});
+	} catch (err) {
+		console.error("Migration failed:", err);
+	} finally {
+		client.release();
+	}
+}
+
+async function main(): Promise<void> {
+	// Only run migrations if `migrate` command used.
+	if (process.argv[2] === "migrate") {
+		const pool = new Pool({ connectionString: config.db.uri });
+		await connectDB(pool);
+		try {
+			await runMigrations(pool);
+		} catch (error) {
+			console.error("Failed to run migrations", error);
+			process.exit(1);
+		}
+		process.exit(0);
+	}
+
+	try {
+		await startServer();
+	} catch (error) {
+		console.error("Failed to start server:", error);
+		process.exit(1);
+	}
+}
+main().catch((err) => {
+	console.error("Failed:", err);
 	process.exit(1);
 });
