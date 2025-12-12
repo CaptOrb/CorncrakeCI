8b28f67da74f158bf2275d5aabe726e4fadc4264
Olivier 'reivilibre <git.contact@librepush.net>
2025-12-12 09:44:45 +0000

    Address feedback from code review
    Signed-off-by: Olivier 'reivilibre <git.contact@librepush.net>

--- server/src/types/gitearepo.ts
+++ server/src/types/gitearepo.ts
@@ -2,16 +2,11 @@ export interface GiteaRepo {
 	id: number;
 	name: string;
 	full_name: string;
-	private: boolean;
 	/* API URL */
 	url: string;
-	html_url: string;
-	clone_url: string;
-	ssh_url: string;
 	owner: {
 		id: number;
 		login: string;
 		avatar_url: string;
 	};
-	description?: string;
 }
